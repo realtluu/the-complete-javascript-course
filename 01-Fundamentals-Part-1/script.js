f6d12c4ce0bb00747f88f8af2ac4c66c833219c4
New Example <realtluu@gmail.com>
2021-08-17 13:29:10 +0900

    Create script.js
    Values and Variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,31 @@
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(1+2+3-4);
+
+////////////////////////////////////
+// Values and Variables
+console.log("tluu");
+console.log(123);
+
+let firstName = "Tluu";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "TL";
+let $function = 19;
+
+let person = "TLUU";
+let PI = 3.141516;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
