4129d0dc97eae2ce7447c334db888eed72fe70c9
New Example <realtluu@gmail.com>
2021-08-20 00:01:10 +0900

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 ////////////////////////////////////
 // Linking a JavaScript File
 let js = "amazing";
@@ -37,3 +38,83 @@ let population = 95.5;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+*/
+
+////////////////////////////////////
+// Data type
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'tluu');
+
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let month;
+console.log(month);
+console.log(typeof month);
+
+month = 8;
+console.log(month);
+console.log(typeof month);
+*/
+
+////////////////////////////////////
+// let, const and var
+
+/*
+let age = 29;
+age = 30;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+var job = 'ds';
+job = 'de';
+
+lastName = 'tluu';
+console.log(lastName);
+*/
+
+////////////////////////////////////
+// basic operations
+
+const now = 2021;
+const age1 = now - 1991;
+const age2 = now - 2020;
+console.log(age1, age2);
+
+// console.log(age1 * 3,  age2 / 5, 3**2);
+
+// const firstName = 'tluu';
+// const lastName = 'de';
+// console.log(firstName + ' ' + lastName);
+
+// let x = 1 + 2;
+// x += 3;
+// x += 5;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// console.log(age1 > age2);
+// console.log(age2 >= 10);
+
+// const isFullAge =  age2 >= 10;
+// console.log(now - 1991 > now - 2020);
+
+console.log(25-10-5);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (age1 + age2)/2;
+console.log(age1, age2, averageAge);
